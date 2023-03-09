126469c1a8b2afad5d060c4ceb3dfbe2992e3a7a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-08 22:49:53 -0800

    Generalized the gene-related functions to work with any identifier.

--- src/fetchAllGenes.js
+++ src/fetchAllGenes.js
@@ -1,78 +1,71 @@
 import { downloader, decompressLines } from "./utils.js";
 
-var gene_symbol_init = false;
-var gene_entrez_init = false;
-var _genes = null;
-
-function add_mapping(gene_data, key) {
-    if (_genes == null) {
-        _genes = [];
-        for (const x of gene_data) {
-            let details = x.split("\t");
-            let obj = { ensembl: details[0] };
-            obj[key] = details.slice(1);
-            _genes.push(obj);
-        }
-    } else {
-        if (gene_data.length !== _genes.length) {
-            throw new Error("discrepancy between length of symbol and entrez mappings");
-        }
-
-        for (var i = 0; i < gene_data.length; i++) {
-            let x = gene_data[i];
-            let existing = _genes[i];
-
-            let details = x.split("\t");
-            if (details[0] !== existing.ensembl) {
-                throw new Error("discrepancy between symbol and entrez mappings for " + details[0]);
-            }
- 
-            existing[key] = details.slice(1);
-        }
-    }
-}
+var _genes = new Map;
 
 /**
+ * @param {string} species - The taxonomy ID of the species of interest, e.g., `"9606"` for human.
  * @param {object} [options={}] - Optional parameters.
- * @param {string} [options.mapping="symbol"] - Which mappings from Ensembl IDs should be retrived.
- * This may be `"symbol"`, `"entrez"` or `"both"`.
+ * @param {?Array} [options.types=null] - Array of strings specifying the identifier types to be retrieved.
+ * The exact choice of strings depends on how the references were constructed.
+ * If `null`, it defaults to an array containing `"symbol"`, `"entrez"` and `"ensembl"`.
  *
- * @return {Array} Array of objects where each object corresponds to a gene and contains the following properties:
+ * @return {Map} Object where each key is named after an identifier type in `types`.
+ * Each value is an array where each element corresponds to a gene and is itself an array of strings containing all identifiers of the current type for that gene.
  *
- * - `ensembl`: string containing the Ensembl ID for the gene.
- * - `symbol`: array containing strings with the gene symbols.
- *   This is only guaranteed to exist if `mapping = "symbol"` or `mapping = "both"`.
- * - `entrez`: array containing strings with the Entrez IDs.
- *   This is only guaranteed to exist if `mapping = "entrez"` or `mapping = "both"`.
- *
- * In the context of **gesel**, the identifier for each gene (i.e., the "gene ID") is encoded as its index in this output array.
+ * The arrays for different identifier types are all of the same length, and corresponding elements across these arrays describe the same gene.
+ * **gesel**'s identifier for each gene (i.e., the "gene ID") is defined as the index of that gene in any of these arrays.
  *
  * @async
  */
-export async function fetchAllGenes({ mapping = "symbol" } = {}) {
-    let symbol = (mapping == "symbol" || mapping == "both");
-    if (symbol && !gene_symbol_init) {
-        var res = await downloader("symbol2gene.tsv.gz")
-        if (!res.ok) {
-            throw "failed to fetch symbol information for genes";
-        }
-        var buffer = await res.arrayBuffer();
-        let gene_data = decompressLines(buffer);
-        add_mapping(gene_data, "symbol");
-        gene_symbol_init = true;
+export async function fetchAllGenes(species, { types = null } = {}) {
+    if (types == null) {
+        types = [ "symbol", "entrez", "ensembl" ];
     }
 
-    let entrez = (mapping == "entrez" || mapping == "both");
-    if (entrez && !gene_entrez_init) {
-        var res = await downloader("entrez2gene.tsv.gz")
-        if (!res.ok) {
-            throw "failed to fetch Entrez information for genes";
-        }
-        var buffer = await res.arrayBuffer();
-        let gene_data = decompressLines(buffer);
-        add_mapping(gene_data, "entrez");
-        gene_entrez_init = true;
+    let target = _genes.get(species);
+    if (typeof target == "undefined") {
+        target = new Map;
+        _genes.set(species, target);
     }
 
-    return _genes;
+    let output = new Map;
+    let promises = [];
+    let processing = [];
+
+    for (const t of types) {
+        let found = target.get(t);
+        if (typeof found == "undefined") {
+            promises.push(downloader(species + "_" + t + ".tsv.gz"));
+            processing.push(t);
+        } else {
+            output.set(t, found);
+        }
+    }
+
+    if (promises.length > 0) {
+        let resolved = await Promise.all(promises);
+        for (var i = 0; i < resolved.length; i++) {
+            let res = resolved[i];
+            if (!res.ok) {
+                throw "failed to fetch symbol information for genes";
+            }
+            var buffer = await res.arrayBuffer();
+
+            let gene_data = decompressLines(buffer);
+            let processed = [];
+            for (const x of gene_data) {
+                if (x == "") {
+                    processed.push([]);
+                } else {
+                    processed.push(x.split("\t"));
+                }
+            }
+
+            let t = processing[i];
+            target.set(t, processed);
+            output.set(t, processed);
+        }
+    }
+
+    return output;
 }
---
+++ src/mapGenesByIdentifier.js
@@ -0,0 +1,53 @@
+import { fetchAllGenes } from "./fetchAllGenes.js";
+
+var by_symbol = new Map;
+var by_symbol_lower = new Map;
+
+/**
+ * @param {string} species - The taxonomy ID of the species of interest, e.g., `"9606"` for human.
+ * @param {string} type - Type of the identifier to use as the key of the map, e.g., `"ensembl"`.
+ * @param {object} [options={}] - Optional parameters.
+ * @param {boolean} [options.lowerCase=false] - Whether to use lower-case keys in the map.
+ *
+ * @return {Map} Map where each key is a string containing a (possibly lower-cased) identifier of the specified `type` and each value is an array.
+ * Each array contains the **gesel** gene IDs associated with the `type` identifier, see {@linkcode fetchAllGenes} for ore details.
+ *
+ * @async
+ */
+export async function mapGenesByIdentifier(species, type, { lowerCase = false } = {}) {
+    let host = (lowerCase ? by_symbol_lower : by_symbol);
+
+    let sfound = host.get(species);
+    if (typeof sfound === "undefined") {
+        sfound = new Map;
+        host.set(species, sfound);
+    }
+
+    let tfound = sfound.get(type);
+    if (typeof tfound === "undefined") {
+        tfound = new Map;
+        sfound.set(type, tfound);
+
+        let _genes = await fetchAllGenes(species, { types: [ type ] }).get(type);
+        for (var i = 0; i < _genes.length; i++) {
+            for (let y of _genes[i].symbol) {
+                if (lowerCase) {
+                    y = y.toLowerCase();
+                }
+
+                let current = tfound.get(y);
+                if (typeof current !== "undefined") {
+                    current.add(i);
+                } else {
+                    tfound.set(y, new Set([i]));
+                }
+            }
+        }
+
+        for (const [key, val] of tfound) {
+            thing.set(key, Array.from(val));
+        }
+    }
+
+    return thing;
+}
